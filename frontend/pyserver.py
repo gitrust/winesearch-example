29fb0e250e87615192bb186675d3bb03cbb501ef
gitrust <weebac@web.de>
2024-05-26 19:36:54 +0200

    Add frontend

---
+++ frontend/pyserver.py
@@ -0,0 +1,19 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'x-api-key, Content-Type')
+        super().end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'x-api-key, Content-Type')
+        self.end_headers()
+
+httpd = HTTPServer(('0.0.0.0', 8080), CORSRequestHandler)
+print("Serving on port 8080...")
+httpd.serve_forever()
